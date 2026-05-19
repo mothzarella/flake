e52f2f90a33035e704b1a2c261622229b6bc754a
tar <git@mothzarella.dev>
2026-05-19 18:49:13 +0200

    fix: update `nix-community` cachix key

--- modules/features/cachix.nix
+++ modules/features/cachix.nix
@@ -8,7 +8,7 @@
       ];
       extra-trusted-public-keys = [
         "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCUSeBc="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
       ];
     };
